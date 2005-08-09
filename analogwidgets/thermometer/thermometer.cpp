625e2737bbad755c5d1d723b4c1ad49eb18c58c4
(no author) <(no author)@9cc7e19c-1ffc-0310-b80c-a714d9727902>
2005-08-09 21:05:23 +0000

    Zrobiony słupek na gradientach - wymaga nadal wiele pracy
    git-svn-id: svn://3e/AnalogWidgets@30 9cc7e19c-1ffc-0310-b80c-a714d9727902

--- analogwidgets/thermometer/thermometer.cpp
+++ analogwidgets/thermometer/thermometer.cpp
@@ -1,8 +1,9 @@
 #include <QtGui>
-#include <math.h>
+#include <cmath>
 #include <assert.h>
 #include "thermometer.h"
 
+using namespace std;
 ThermoMeter::ThermoMeter(QWidget *parent)
         : QMyAbstractMeter(parent)
 {
@@ -46,7 +47,7 @@ void ThermoMeter::paintBackground(QPainter & painter)
 
 	// pocz±tek ³uku dolnego po lewej stronie
 	glass.moveTo(12.5,267.5);
-	glass.quadTo(12.5,264.0, 7.5,259.0);
+	glass.quadTo(12.5,263.0, 7.5,259.0);
 
 	glass.lineTo(7.5,25.0);
 
@@ -55,7 +56,7 @@ void ThermoMeter::paintBackground(QPainter & painter)
 	glass.quadTo(-7.5,12.5,-7.5,25.0);
 	glass.lineTo(-7.5,259.0);
 	// tutaj musi byæ ³uk dolny o promineniu 25
-	glass.quadTo(-12.5,264.0, -12.5,267.5);
+	glass.quadTo(-12.5,263.0, -12.5,267.5);
 	glass.quadTo(-12.5,280.0,  0.0,280.0);
 	glass.quadTo( 12.5,280.0,  12.5,267.5);
 
@@ -82,30 +83,55 @@ void ThermoMeter::paintBackground(QPainter & painter)
 
 }// paintBackground
 
+// Offset by lepiej by³o widaæ o co z nim biega - mówi±c oglêdnie offset to miejsce
+// poni¿ej zera od którego zaczynamy rysowaæ s³upek rtêci.
+#define OFFSET 10
+
 void ThermoMeter::paintEvent(QPaintEvent * )
 {
-
+      // Inicjalizacja paintera
 	QPainter painter(this);
         initCoordinateSystem(painter);
       // --------------------------------------------- ///
-	QBrush brush;
-	brush.setColor(Qt::blue);
-        if (m_value >= m_nominal )
-	    brush.setColor(Qt::green); 
-	
-	if (m_value >= m_critical)
-	   { 
-		brush.setColor(Qt::red); 
-	//	m_valueFont.setColor(Qt::red); 
-	   }
-	painter.setPen(Qt::NoPen); 
-	painter.setBrush(brush);
+       // Dobór colorów do rysowania
+	QColor color=Qt::blue;
+	if (m_value >= m_nominal )
+	    color=Qt::green;
 
+	if (m_value >= m_critical)
+	   {
+		color=Qt::red;
+	     	painter.setPen(color); // by potem temp podaæ na czerwono
+	   }
+
+	QLinearGradient slupek(0.0,0.0,5.0,0.0);
+	QRadialGradient zbiornik(0.0,0.0, 5.0,5.0);
+
+	slupek.setSpread(QGradient::ReflectSpread);
+
+	color.setHsv(color.hue(),color.saturation(),color.value());
+	slupek.setColorAt(1.0,color);
+	zbiornik.setColorAt(1.0,color);
+
+	color.setHsv(color.hue(),color.saturation()-200,color.value());
+	slupek.setColorAt(0.0,color);
+	zbiornik.setColorAt(0.0,color);
+
+        // Wyznaczenie wysoko¶ci s³upka;
 	double factor =  m_value - m_min;
 	       factor /= m_max - m_min;
-	 
-	int temp = static_cast<int> (231.0 *  factor);
-	painter.drawRect(-5,259 ,10, 30);
+	int temp = static_cast<int> (224.0 *  factor), height = temp + OFFSET;
+	if (231 < temp  ) height = 231 + OFFSET; // by rtêæ dosz³a do koñca terometra ale go nie rozwali³a
+	if (OFFSET-5 >= height )  height = OFFSET-5; // by nie zeszlo poni¿ej pieciu "pixeli" poni¿ej zera
+	// Narysowanie s³upka  wraz z zbiorniczkiem rtêci
+        painter.setPen(Qt::NoPen);
+	painter.setBrush(slupek);
+        painter.drawRect(-5,252+OFFSET - height ,10, height);
+	painter.setBrush(zbiornik);
+	painter.drawEllipse(-10,258,20,20);
+
+	// Na³o¿enie szklanej bañki
 	doUpdateBackground();
-	//painter.drawLine(0,0,300,300);
+
 }// paintEvent
+#undef OFFSET
